6cac8b4eb6c385f0569c138b7e8069c810b22c2e
Mikhail <threedmix9992828@gmail.com>
2025-09-28 15:27:15 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double sum = 0;
+        
+        while (true)
+        {
+            Console.Write("Введите число:");
+            string input = Console.ReadLine();
+
+            double number;
+            bool isSuccess = double.TryParse(input, out number);
+
+            if (isSuccess)
+            {
+                if (number != 0)
+                {
+                    sum += number;
+                    Console.WriteLine($"Сумма: {sum}");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверный формат");
+            }
+            
+        }
+
+        
+    }
+}
