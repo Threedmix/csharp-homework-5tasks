78b4cab82e6a6907752972f6fff8f3490d429f14
Mikhail <threedmix9992828@gmail.com>
2025-09-28 14:55:51 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите температуру в °C:");
+        string input = Console.ReadLine();
+
+        double celsius;
+        bool isSuccess = double.TryParse(input, out celsius);
+
+        if (isSuccess)
+        {
+             double fahrenheit = (celsius * 9/5) + 32;
+             Console.WriteLine($"{celsius}°C = {fahrenheit}°F");   
+        }
+        else
+        {
+            Console.WriteLine("Неверный формат");
+        }
+        
+    }
+}
